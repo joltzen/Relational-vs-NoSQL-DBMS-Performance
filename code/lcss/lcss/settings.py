0d2792b8f48128277fad8824bbb39e528cea03f4
joltzen <jason.oltzen@smail.th-koeln.de>
2023-06-07 09:56:37 +0200

    Connect postgresql

--- code/lcss/lcss/settings.py
+++ code/lcss/lcss/settings.py
@@ -76,14 +76,23 @@ WSGI_APPLICATION = "lcss.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
-
+"""
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "coupon",
+        "USER": "joltzen",
+        "PASSWORD": "lcssteam07",
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
